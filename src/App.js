9ec4984e76b46385e0fc7e36c6eb4823dbd9a652
akeros <mixali4kin@bk.ru>
2021-12-10 17:10:56 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import Vector from './Vector.svg';
+import pub from './pub.svg';
+import line from './line.svg';
+import card from './card.svg';
+import cardtwo from './cardtwo.svg';
+import cardblack from './cardblack.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main">
+      <div className="razmet">
+      <div className="wrapper">
+          <div className="text">Главная</div>
+          <div className="date">17 июля 2021</div>
+      </div>
+      <div>
+        <div className="logo-three"><img src={line}/></div>
+          <div className="logo"><img src={Vector}/></div>
+          <div className="logo-two"><img src={pub}/></div>
+      </div>
+      </div>
+      <div className="Breadcrumbs">
+        <div className="Breadcrumbs-text">Новинки</div>
+        <div className="Breadcrumbs-text">Сладкие</div>
+        <div className="Breadcrumbs-text">Хит</div>
+        <div className="Breadcrumbs-text">Крепкие</div>
+        <div className="Breadcrumbs-text">Лонг</div>
+        <div className="Breadcrumbs-text">Шот</div>
+      </div>
+      <div className="card">
+        <img src={card}/>
+          <div className="cardblack">
+          <img src={cardblack}/>
+          <div className="cardblacktext">30%</div>
+          <div className="cardblacktextalc">Алкоголь</div>
+          </div>
+      </div>
+          <div className="cardrazmet">
+            <div className="cardtext">Blackberry</div>
+            <div className="cardtexttwo">Fresh drink</div>
+          </div>
+      <div className="card-two">
+        <img src={cardtwo}/>
+        <div className="cardblack">
+          <img src={cardblack}/>
+          <div className="cardblacktext">10%</div>
+          <div className="cardblacktextalc">Алкоголь</div>
+          </div>
+      </div>
+      <div className="cardrazmettwo">
+            <div className="cardtext">Blackberry</div>
+            <div className="cardtexttwo">Fresh drink</div>
+      </div>      
+     </div>
   );
 }
 
