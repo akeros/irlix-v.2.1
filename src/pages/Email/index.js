02f4d4a5756a317ca52bbc81a2bd1023ac941b0e
akeros <mixali4kin@bk.ru>
2021-12-24 15:12:57 +0400

    Added zakladki

--- src/pages/Email/index.js
+++ src/pages/Email/index.js
@@ -1,12 +1,35 @@
 import './index.css';
+import { useSelector } from "react-redux";
+import {cardArray} from '../../mock/cards';
+import Header from '../../components/Header';
+import Card from '../../components/Card';
+import Footer from '../../components/Header';
 
 
 function Email() {
-    return (
-        <>
-      
-      </>
-    );
-  }
+  const favorites = useSelector((state => state.app.favorites));
+
+ 
+     return (
+        <div className="main">
+          <Header/>
+          <main>
+            <div className="cards-list">
+              {cardArray.filter(card => favorites.includes(card.id)).map((item) => (
+                <Card
+                  img={item.img}
+                  percents={item.percents}
+                  title={item.title}
+                  description={item.description}
+                  key={item.id}
+                  id={item.id}
+                />
+              ))}
+            </div>
+          </main>
+          <Footer/>
+         </div> 
+  )
+}
   
   export default Email;
